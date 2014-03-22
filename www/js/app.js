d552608b594816e0a34a1e46f739e71adce65704
Al Scheuring <alberts@Al-Scheurings-MacBook-Pro.local>
2014-03-21 20:49:13 -0400

    x

--- www/js/app.js
+++ www/js/app.js
@@ -21,7 +21,7 @@ function getBrewers(){
 }
     
 	function untappdLaunch(id){
-	window.plugins.webintent.startActivity({
+window.plugins.webintent.startActivity({
       action: window.plugins.webintent.ACTION_VIEW,
       url: 'untappd://?beer='+ id ,
     },
@@ -71,19 +71,17 @@ $(document).on("pagebeforeshow", "#brewer-detail", function () {
 		    info_view += '<h3>' + info.Brewer.name +'</h3>';
 		    info_view += '<h4><i>' + info.Brewer.brewery +'</i></h4>';
    
-   		  if(info.Brewer.untappd_id){
-     //   	if(device.platform == "Android"){
-//           info_view += '<a href="#" onclick="untappdLaunch(info.Brewer.untappd_id)" target="_blank" data-rel="popup" data-role="button" data-inline="true" data-mini="true" data-transition="pop">Android Checkin on UNTAPPD</a>';
-		   // info_view += '<a href="#" data-role="button" data-mini="true" ontouchstart="untappdLaunch('+ info.Brewer.id +')">Android Untappd </a>';
+   /*		  if(info.Brewer.untappd_id){
+        	if(device.platform == "Android"){
 
-       //    info_view += '<a href="#" ontouchend("untappdLaunch('+ info.Brewer.untappd_id + '')" target="_blank" data-rel="popup" data-role="button" data-inline="true" data-mini="true" data-transition="pop">Checkin on UNTAPPD</a>';
-		//	}
-			//if(device.platform == "iPhone" || device.platform == "iOS"){
-           //info_view += '<a href="untappd://?brewery='+ info.Brewer.untappd_id +'" target="_blank" data-rel="popup" data-role="button" data-inline="true" data-mini="true" data-transition="pop">IOS Checkin on UNTAPPD</a>';
-		
-			//}
+           info_view += '<a href="#" ontouchend("untappdLaunch('+ info.Brewer.untappd_id + '')" target="_blank" data-rel="popup" data-role="button" data-inline="true" data-mini="true" data-transition="pop">Checkin on UNTAPPD</a>';
+			}
+			if(device.platform == "iPhone" || device.platform == "iOS"){
+           info_view += '<a href="untappd://?brewery='+ info.Brewer.untappd_id +'" target="_blank" data-rel="popup" data-role="button" data-inline="true" data-mini="true" data-transition="pop">Checkin on UNTAPPD</a>';
 		
 			}
+		
+			} */
 		    info_view += '</div>';
 
 			var info_view2 = "";

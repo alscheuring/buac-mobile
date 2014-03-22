446ea60ed318ee2f041b66b1b40550a78948b30f
Al Scheuring <alberts@Al-Scheurings-MacBook-Pro.local>
2014-03-22 19:30:01 -0400

    x

--- www/js/index.js
+++ www/js/index.js
@@ -218,17 +218,17 @@ var app = {
 
 
 
-        app.receivedEvent('deviceready');
+      //  app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
+   /*     var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+        console.log('Received Event: ' + id);*/
     }
 };

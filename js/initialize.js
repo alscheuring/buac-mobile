ff419ecde57bb5684711c9b1b2c13128c028bf98
Al Scheuring <alberts@Al-Scheurings-MacBook-Pro.local>
2014-03-23 16:47:40 -0400

    x

---
+++ js/initialize.js
@@ -0,0 +1,52 @@
+
+	
+	document.addEventListener("deviceready", onDeviceReady, false);
+
+    // PhoneGap is loaded and it is now safe to make calls PhoneGap methods
+    //
+    function onDeviceReady() {
+    FastClick.attach(document.body);
+    getBeers();
+    getBrewers();
+    initPushwoosh();
+
+
+}
+	
+	
+function initPushwoosh()
+{
+	alert("pushwoosh called");
+    var pushNotification = window.plugins.pushNotification;
+    pushNotification.onDeviceReady();
+ 
+    pushNotification.registerDevice({ projectid: "509023216724", appid : "3C2AE-AEC40" },
+        function(status) {
+        	alert("pushnotification status " + status);
+            var pushToken = status;
+            console.warn('push token: ' + pushToken);
+        },
+        function(status) {
+            console.warn(JSON.stringify(['failed to register ', status]));
+        }
+    );
+ 
+    document.addEventListener('push-notification', function(event) {
+        var title = event.notification.title;
+            var userData = event.notification.userdata;
+                                 
+            if(typeof(userData) != "undefined") {
+            console.warn('user data: ' + JSON.stringify(userData));
+        }
+                                     
+        navigator.notification.alert(title);
+    });
+}	
+	
+	
+	
+	/*Fixes overlap on front page header */
+$(window).on('load', function () {
+    $(this).trigger('resize');
+});
+

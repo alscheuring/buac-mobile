78b856fdc08845288bb1b99ec213debdd739c466
Al Scheuring <alberts@vestarkmacbookprov2.bio.cmu.edu>
2014-03-25 12:24:11 -0400

    x

--- js/index.js
+++ js/index.js
@@ -217,8 +217,9 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        navigator.splashscreen.hide();
         initPushwoosh();
+        navigator.splashscreen.hide();
+
        
     },
     // Update DOM on a Received Event
